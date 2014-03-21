a4c89298f2b7f3d134b982200604690063cf3673
ftobia <frank.tobia@gmail.com>
2014-03-20 22:38:52 -0400

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name='pytest-ordering',
     description='pytest plugin to run your tests in a specific order',
-    version='0.1',
+    version='0.2',
     author='Frank Tobia',
     author_email='frank.tobia@gmail.com',
     url='https://github.com/ftobia/pytest-ordering',
