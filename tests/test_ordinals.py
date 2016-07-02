b7a8d8fda27075406e29d51a644e8c1ee4217fcb
ftobia <frank.tobia@gmail.com>
2016-07-02 16:05:32 -0400

    Tests for ordinal behavior (#14). Currently failing.

--- tests/test_ordinals.py
+++ tests/test_ordinals.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-def test_first_mark(item_names_for):
+
+def test_first(item_names_for):
     tests_content = """
     import pytest
 
@@ -9,11 +10,57 @@ def test_first_mark(item_names_for):
     @pytest.mark.first
     def test_2(): pass
     """
-
     assert item_names_for(tests_content) == ['test_2', 'test_1']
 
 
-def test_last_mark(item_names_for):
+def test_second(item_names_for):
+    tests_content = """
+    import pytest
+
+    def test_1(): pass
+    def test_2(): pass
+    def test_3(): pass
+    def test_4(): pass
+
+    @pytest.mark.second
+    def test_5(): pass
+    """
+    assert item_names_for(tests_content) == ['test_1', 'test_5', 'test_2', 'test_3', 'test_4']
+
+
+def test_third(item_names_for):
+    tests_content = """
+    import pytest
+
+    def test_1(): pass
+    def test_2(): pass
+    def test_3(): pass
+
+    @pytest.mark.third
+    def test_4(): pass
+
+    def test_5(): pass
+    """
+    assert item_names_for(tests_content) == ['test_1', 'test_2', 'test_4', 'test_3', 'test_5']
+
+
+def test_second_to_last(item_names_for):
+    tests_content = """
+    import pytest
+
+    def test_1(): pass
+
+    @pytest.mark.second_to_last
+    def test_2(): pass
+
+    def test_3(): pass
+    def test_4(): pass
+    def test_5(): pass
+    """
+    assert item_names_for(tests_content) == ['test_1', 'test_3', 'test_4', 'test_2', 'test_5']
+
+
+def test_last(item_names_for):
     tests_content = """
     import pytest
 
@@ -22,11 +69,10 @@ def test_last_mark(item_names_for):
 
     def test_2(): pass
     """
-
     assert item_names_for(tests_content) == ['test_2', 'test_1']
 
 
-def test_first_last_marks(item_names_for):
+def test_first_last(item_names_for):
     tests_content = """
     import pytest
 
@@ -38,5 +84,4 @@ def test_first_last_marks(item_names_for):
 
     def test_3(): pass
     """
-
     assert item_names_for(tests_content) == ['test_2', 'test_3', 'test_1']
